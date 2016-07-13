5aeab1092961a5dffbed4b79c09097bbb784c5bd
Jace Ju <jaceju@kkbox.com>
2016-07-13 11:15:45 +0800

    Add first test case

---
+++ tests/FruitsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FruitsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
