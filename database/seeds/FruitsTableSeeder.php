1587567d7ef1cb4669eb0dbba0d6916c3969c12d
Jace Ju <jaceju@kkbox.com>
2016-07-14 23:40:05 +0800

    Add model / migration / seeder

---
+++ database/seeds/FruitsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Fruit;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FruitsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+        DB::table('fruits')->delete();
+        $fruits = [
+            ['name' => 'apple', 'color' => 'green', 'weight' => 150, 'delicious' => true],
+            ['name' => 'banana', 'color' => 'yellow', 'weight' => 116, 'delicious' => true],
+            ['name' => 'strawberries', 'color' => 'red', 'weight' => 12, 'delicious' => true],
+        ];
+        // Loop through fruits above and create the record in DB
+        foreach ($fruits as $fruit) {
+            Fruit::create($fruit);
+        }
+        Model::reguard();
+    }
+}
